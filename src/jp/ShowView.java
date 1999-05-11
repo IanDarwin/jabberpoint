ee4c4f510d4b01972aa513d3d84e51b7cfee6198
IanDarwin <>
1999-05-11 19:46:53 +0000

    Quite a bit of evolution. Found on darian in this form.

--- src/jp/ShowView.java
+++ src/jp/ShowView.java
@@ -2,16 +2,34 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-/** JabberPoint View */
-public class View extends Component implements Observer {
+/** JabberPoint Show View */
+public class ShowView extends Component implements Observer {
 	/** The collection of M's on the current page */
 	Slide slide;
+	/** Our preferred Width */
+	int prefWidth;
+	/** Our preferred Height */
+	int prefHeight;
+	/** The label font */
+	Font labelFont = null;
+	/** The Model */
+	Model model = null;
 
-	/** Construct a View */
-	View() {
+	/** Construct a ShowView */
+	ShowView() {
 		setBackground(Color.white);
 		Dimension them = Toolkit.getDefaultToolkit().getScreenSize();
-		setBounds(0, 0, them.width, them.height);
+		prefWidth = them.width;
+		prefHeight = them.height;
+		labelFont = new Font("Dialog", Font.BOLD, 10);
+	}
+
+	public Dimension getPreferredSize() {
+		if (prefWidth>800)
+			prefWidth = 800;
+		if (prefHeight>600)
+			prefHeight = 600;
+		return new Dimension(prefWidth, prefHeight);
 	}
 
 	public void update(Observable model, Object data) {
@@ -19,6 +37,7 @@ public class View extends Component implements Observer {
 			throw new IllegalArgumentException("Wimpout 1");
 		if (!(data instanceof Slide))
 			throw new IllegalArgumentException("Observable gave bad Slide");
+		model = (Model)model;
 		slide = (Slide)data;
 		repaint();
 	}
@@ -28,15 +47,19 @@ public class View extends Component implements Observer {
 		int y = 75;
 		int indent;
 		if (slide == null) {
-			// System.err.println("View.paint called while slide is null");
+			// System.err.println("ShowView.paint called while slide is null");
 			return;
 		}
 		Vector v = slide.getMs();
 		if (v == null) {
-			System.err.println("View.paint: getMs() yields null");
+			System.err.println("ShowView.paint: getMs() yields null");
 			return;
 		}
+		if (model != null)
+			g.drawString("Slide " + model.getCurrentNumber() + " of " + model.getSize(),
+				getSize().width-150, getSize().height-30);
 		for (int i=0; i<v.size(); i++) {
+			g.setFont(labelFont);
 			M m = (M)v.elementAt(i);
 			// System.out.println(m);
 			Style s = JabberPoint.styles[m.level];
@@ -54,7 +77,7 @@ public class View extends Component implements Observer {
 					break;
 				default:
 					throw new IllegalArgumentException(
-					"Unknown type (" + m.type + ") found in View.paint()");
+					"Unknown type (" + m.type + ") found in ShowView.paint()");
 			}
 		}
 	}
